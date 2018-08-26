f0f3f9c8d41f233d80ffd2840445dad0f07bd8b4
Julian Gieseke <julian.gieseke@gmail.com>
2018-08-26 18:41:04 +0200

    refactor(tests): improve test cases

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,59 +1,55 @@
 const {
   verifyConditionsErrorMessage,
   verifyConditions,
-  notify
+  success
 } = require("../index.js");
-
-// this helps a lot: https://github.com/felixfbecker/semantic-release-firefox/blob/master/src/semantic-release.ts
-const defaultContext = {
-  env: {},
-  logger: { log: console.log, error: console.error, success: console.log }
-};
-
-const pluginConfig = {};
 
 describe("verifyConditions", () => {
   it("throws without env vars", async () => {
-    expect.assertions(1);
-    try {
-      await verifyConditions({}, { env: {} });
-    } catch (e) {
-      expect(e).toEqual(new Error(verifyConditionsErrorMessage));
-    }
+    await expect(verifyConditions({}, { env: {} })).rejects.toThrowError(
+      verifyConditionsErrorMessage
+    );
   });
 
   it("throws with missing password env var", async () => {
-    expect.assertions(1);
-    try {
-      await verifyConditions({}, { env: { JIRA_USER: "Bender" } });
-    } catch (e) {
-      expect(e).toEqual(new Error(verifyConditionsErrorMessage));
-    }
+    await expect(
+      verifyConditions({}, { env: { JIRA_USER: "Bender" } })
+    ).rejects.toThrowError(verifyConditionsErrorMessage);
   });
 
   it("throws with missing user env var", async () => {
-    expect.assertions(1);
-    try {
-      await verifyConditions({}, { env: { JIRA_PASS: "K1ll-aLL-hum4nz!" } });
-    } catch (e) {
-      expect(e).toEqual(new Error(verifyConditionsErrorMessage));
-    }
+    await expect(
+      verifyConditions({}, { env: { JIRA_PASS: "K1ll-aLL-hum4nz!" } })
+    ).rejects.toThrowError(verifyConditionsErrorMessage);
+  });
+
+  it("throws with empty password env var", async () => {
+    await expect(
+      verifyConditions({}, { env: { JIRA_USER: "" } })
+    ).rejects.toThrowError(verifyConditionsErrorMessage);
+  });
+
+  it("throws with empty user env var", async () => {
+    await expect(
+      verifyConditions({}, { env: { JIRA_PASS: "" } })
+    ).rejects.toThrowError(verifyConditionsErrorMessage);
   });
 
   it("succeeds with user and pass env vars", async () => {
-    try {
-      await verifyConditions(
+    await expect(
+      verifyConditions(
         {},
         { env: { JIRA_USER: "Bender", JIRA_PASS: "K1ll-aLL-hum4nz!" } }
-      );
-    } catch (e) {
-      expect(e).not.toEqual(new Error(verifyConditionsErrorMessage));
-    }
+      )
+    ).resolves;
   });
 });
 
-describe("notify", () => {
+describe("success", () => {
   it("initializes", () => {
-    expect(() => notify(pluginConfig, defaultContext)).not.toThrow();
+    const loggerSuccess = jest.fn();
+    expect(() =>
+      success({}, { logger: { success: loggerSuccess } })
+    ).not.toThrow();
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const verifyConditionsErrorMessage =
-  "Environment variables JIRA_USER and JIRA_PASS must be set";
+  "Environment variables JIRA_USER and JIRA_PASS must be set and not empty";
 
 async function verifyConditions(_pluginConfig, context) {
   const {
     env: { JIRA_USER, JIRA_PASS }
   } = context;
+
   if (!JIRA_USER || !JIRA_PASS) {
     throw new Error(verifyConditionsErrorMessage);
   }
 }
 
-async function notify(pluginConfig, context) {
-  context.logger(pluginConfig, context);
+async function success(pluginConfig, context) {
+  const { logger } = context;
+
+  logger.success(pluginConfig, context);
 }
 
-module.exports = { verifyConditionsErrorMessage, verifyConditions, notify };
+module.exports = { verifyConditionsErrorMessage, verifyConditions, success };

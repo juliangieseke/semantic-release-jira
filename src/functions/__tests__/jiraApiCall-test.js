4dbe935a4f0d877d4cbdeceb783fb18e201b5e00
Julian Gieseke <julian.gieseke@gmail.com>
2020-09-20 19:29:40 +0200

    fix: handle null/empty responses

--- src/functions/__tests__/jiraApiCall-test.js
+++ src/functions/__tests__/jiraApiCall-test.js
@@ -8,9 +8,10 @@ const version = "1.1.1";
 const authHeader = "Token foobar==";
 const action = {
   method: "POST",
-  url: "https://jira.example.com/rest/api/2/versions",
+  url: "https://jira.example.com/rest/api/2/version",
   body:
     '{ "name": "FooComponent ${version}", "archived": false, "released": true, "project": "${project}"}',
+  ignoreErrors: ["A version with this name already exists in this project."],
 };
 const logger = {
   success: () => {},
@@ -34,7 +35,55 @@ describe("jiraApiCall", () => {
       })
     ).toEqual({ json: true });
     expect(successLogger).toHaveBeenCalledWith(
-      `[FOO-12345] Action "https://jira.example.com/rest/api/2/versions" done.`
+      `[FOO-12345] Action "https://jira.example.com/rest/api/2/version" done.`
+    );
+  });
+  it("handles null response", async () => {
+    const successLogger = jest.fn();
+    expect.assertions(2);
+    fetch.mockResponse(null);
+    expect(
+      await jiraApiCall({
+        issueKey,
+        authHeader,
+        version,
+        action,
+        logger: { ...logger, success: successLogger },
+      })
+    ).toEqual(
+      "FetchError: invalid json response body at undefined reason: Unexpected end of JSON input"
+    );
+    expect(successLogger).toHaveBeenCalledWith(
+      `[FOO-12345] Action "https://jira.example.com/rest/api/2/version" done.`
+    );
+  });
+  it("ignores set errors", async () => {
+    const successLogger = jest.fn();
+    expect.assertions(2);
+    fetch.mockResponse(
+      JSON.stringify({
+        errorMessages: [],
+        errors: {
+          name: "A version with this name already exists in this project.",
+        },
+      })
+    );
+    expect(
+      await jiraApiCall({
+        issueKey,
+        authHeader,
+        version,
+        action,
+        logger: { ...logger, success: successLogger },
+      })
+    ).toEqual({
+      errorMessages: [],
+      errors: {
+        name: "A version with this name already exists in this project.",
+      },
+    });
+    expect(successLogger).toHaveBeenCalledWith(
+      `[FOO-12345] Action "https://jira.example.com/rest/api/2/version" done.`
     );
   });
 });
